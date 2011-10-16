28b0bb11278b48728e10f0f156b3013272fcceff
Daniel Morgan <me@danielmorgan.co.uk>
2011-10-16 18:55:30 +0100

    Duplicated backups/save functoin for cron use

--- app/controllers/backups_controller.php
+++ app/controllers/backups_controller.php
@@ -77,6 +77,59 @@ class BackupsController extends AppController {
 		$this->Session->setFlash(__('Database backup created', true));
 		$this->redirect(array('action' => 'index'));
 	}
+
+	function cron_save($tables = '*') {
+		$this->autoRender = false;
+		App::import('Core', 'ConnectionManager');
+		$dataSource = ConnectionManager::getDataSource('default');	
+		$host = $dataSource->config['host'];
+		$user = $dataSource->config['login'];
+		$pass = $dataSource->config['password'];
+		$name = $dataSource->config['database'];
+	
+		$link = mysql_connect($host,$user,$pass);
+		mysql_select_db($name,$link);
+
+		if($tables == '*') {
+			$tables = array();
+			$result = mysql_query('SHOW TABLES');
+			while($row = mysql_fetch_row($result)) {
+				$tables[] = $row[0];
+			}
+		}
+		else {
+			$tables = is_array($tables) ? $tables : explode(',',$tables);
+		}
+		
+		$return = null;
+		foreach($tables as $table) {
+			$result = mysql_query('SELECT * FROM '.$table);
+			$num_fields = mysql_num_fields($result);
+
+			$return.= 'DROP TABLE '.$table.';';
+			$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+			$return.= "\n\n".$row2[1].";\n\n";
+
+			for ($i = 0; $i < $num_fields; $i++) {
+				while($row = mysql_fetch_row($result)) {
+					$return.= 'INSERT INTO '.$table.' VALUES(';
+					for($j=0; $j<$num_fields; $j++) {
+						$row[$j] = addslashes($row[$j]);
+						$row[$j] = ereg_replace("\n","\\n",$row[$j]);
+						if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+						if ($j<($num_fields-1)) { $return.= ','; }
+					}
+					$return.= ");\n";
+				}
+			}
+			
+			$return.="\n\n\n";
+		}
+		
+		$handle = fopen(ROOT.DS."app".DS."backups".DS.'backup-'.time().'.sql','w+');
+		fwrite($handle,$return);
+		fclose($handle);
+	}
 	
 	function restore($file) {
 		$this->autoRender = false;
